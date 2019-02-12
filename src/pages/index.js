7d6edefaa4df1eca120716e75049ab37b9bafe8d
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-02-12 14:19:36 -0700

    Show recipes names on the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
-import React from "react"
+import React from 'react';
+import { graphql } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+export default ({ data }) =>
+  data.allRecipesJson.edges.map(({ node }) => (
+    <div key={node.id}>{node.name}</div>
+  ));
+
+export const query = graphql`
+  query indexQuery {
+    allRecipesJson {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
