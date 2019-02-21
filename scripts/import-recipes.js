b24812bf6be3cad5e5f9736581059905874f2e33
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-02-20 23:21:48 -0700

    gst

--- scripts/import-recipes.js
+++ scripts/import-recipes.js
@@ -3,13 +3,20 @@ require('dotenv').config();
 const paprika = require('export-paprika-recipes');
 
 const dir = 'data';
+const photoDir = `./${dir}/photos`;
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
 
+if (!fs.existsSync(photoDir)) {
+  fs.mkdirSync(photoDir);
+}
+
+console.log(photoDir);
 paprika.saveRecipes(
   process.env.PAPRIKA_USERNAME,
   process.env.PAPRIKA_PASSWORD,
-  `./${dir}/recipes.json`
+  `./${dir}/recipes.json`,
+  photoDir
 );
